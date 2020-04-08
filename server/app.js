84118f89a9d625bc2f2ea9457618f9bfd2deb168
Anna Kryva <anna120100@gmail.com>
2020-04-08 13:29:48 +0300

    Added starter file

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+const logging = require('./logs/log');
+
+const app = express();
+
+app.use(morgan('tiny'));
+app.use(express.json({extended: true}));
+
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/profiles/self', require('./routes/profiles.routes'));
+app.use('/api/photo/self', require('./routes/photo.routes'));
+app.use('/api/trucks', require('./routes/trucks.routes'));
+app.use('/api/loads', require('./routes/loads.routes'));
+
+const PORT = config.get('port') || 5000;
+
+async function start() {
+  try {
+    await mongoose.connect(config.get('mongoUri'), {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    app.listen(PORT, ()=> console.log(`App has been started on port ${PORT}`));
+  } catch (error) {
+    logging('Error', error.message);
+    process.exit(1);
+  }
+}
+
+start();
