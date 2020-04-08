7b18b789eaca8fc614b8e5808634bcab25a4a7f8
Anna Kryva <anna120100@gmail.com>
2020-04-08 13:25:09 +0300

    Added configuration

---
+++ config/custom-environment-variables.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: "PORT",
+  jwtSecret: "TOKEN_SECRET",
+  mongoUri: "MONGO_URI",
+  awsBucketName: "AWS_BUCKET_NAME",
+  awsAccessKeyId: "AWS_ACCESS_KEY_ID",
+  awsSecretAccessKey: "AWS_SECRET_ACCESS_KEY",
+  awsRegion: "AWS_REGION",
+  awsUploadedFileUrlLink: "AWS_UPLOADED_FILE_URL_LINK",
+  emailAddress: "EMAIL_ADDRESS",
+  emailPassword: "EMAIL_PASSWORD"
+};
---
+++ config/default.js
@@ -0,0 +1,12 @@
+module.exports = {
+  port: 3000,
+  jwtSecret: "secret",
+  mongoUri: "mongodb+srv://<username>:<password>@uberservice-d5b8s.mongodb.net/UberServiceDB?retryWrites=true&w=majority",
+  awsBucketName: "bucket-name",
+  awsAccessKeyId: "awsAccessKeyId",
+  awsSecretAccessKey: "awsSecretAccessKey",
+  awsRegion: "aws-region",
+  awsUploadedFileUrlLink: "https://aws-region.amazonaws.com/bucket-name/",
+  emailAddress: "user@example.com",
+  emailPassword: "password"
+};
